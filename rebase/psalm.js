d99706d4d9223c58fe03653b36b726b497f4d628
BrownieMaar <barnamarci7@gmail.com>
2022-08-08 15:15:02 +0200

    format psalms

--- rebase/psalm.js
+++ rebase/psalm.js
@@ -1,9 +1,34 @@
 const psalmArray = document.getElementsByClassName("psalm");
 
+function makeBold(str) {
+    while (str.indexOf("__") !== -1) {
+        console.log("Found one!");
+        let firstIndex = str.indexOf("__")
+        let secondIndex = str.indexOf("__", firstIndex + 1);
+        console.log(`${firstIndex}, ${secondIndex}`);
+        let inside = str.slice(firstIndex+2, secondIndex);
+        inside = `<b>${inside}</b>`;
+        console.log(inside);
+        str = str.slice(0, firstIndex) + inside + str.slice(secondIndex + 2, -1);
+    }
+    return str;
+}
+
+function makeItalics(str) {
+    while (true) {
+        if (str.indexOf("_") === -1 || str.indexOf("_") === str.indexOf("__") && str.indexOf("_", str.indexOf("__") + 2)) {
+            break;
+        }
+        
+    }
+    return str;
+}
+
 for (let psalmElement of psalmArray) {
     const psalmTextArray = psalmElement.innerHTML.split("\n");
     if (psalmTextArray[0] === "") psalmTextArray.shift();
     psalmElement.innerHTML = "";
+    
     for (let i = 0; i < psalmTextArray.length; i++) {
         let isItAFirstLine = false;
         if (i === 0) {
@@ -17,6 +42,10 @@ for (let psalmElement of psalmArray) {
         ) {
             isItAFirstLine = true;
         }
+
+        psalmTextArray[i] = makeItalics(psalmTextArray[i]);
+        
+        psalmTextArray[i] = makeBold(psalmTextArray[i]);
 
         psalmTextArray[i] = psalmTextArray[i].replace("†","<span class='rubrum'>†</span>")
 
@@ -37,8 +66,6 @@ for (let psalmElement of psalmArray) {
         if (isItAFirstLine) {
             psalmTextArray[i] = `<strong>${psalmTextArray[i][0]}</strong>${psalmTextArray[i].slice(1, -1)}`
         }
-
-        console.log(i + psalmTextArray[i]);
         psalmRowElement.innerHTML = psalmTextArray[i];
         if (!isItAFirstLine) psalmRowElement.classList.add("intend");
         psalmElement.appendChild(psalmRowElement);
